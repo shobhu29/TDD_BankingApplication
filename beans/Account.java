9160aff897b6092ce4c6055a1903c2b6c42fd3d0
shobhu29 <46920235+shobhu29@users.noreply.github.com>
2019-02-04 17:12:32 +0530

    Add files via upload

---
+++ beans/Account.java
@@ -0,0 +1,46 @@
+package com.capgemini.beans;
+
+public class Account {
+
+	
+	private int account_num;
+	private int amount;
+	
+	public int getAccount_num() {
+		return account_num;
+	}
+	public void setAccount_num(int account_num) {
+		this.account_num = account_num;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + account_num;
+		result = prime * result + amount;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		if (account_num != other.account_num)
+			return false;
+		if (amount != other.amount)
+			return false;
+		return true;
+	}
+	
+	
+}
